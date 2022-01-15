f67913b16693f38b3d951d9ecf09af04d3e07484
kwayebopp <kwasi@epihub.com>
2022-01-15 16:47:09 -0500

    basic dumb test

--- test/cyclone_test.rb
+++ test/cyclone_test.rb
@@ -4,11 +4,16 @@
 require "test_helper"
 
 class CycloneTest < Minitest::Test
+  def multi_list
+    [(1..3).to_a, (4..6).to_a]
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Cyclone::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_concat
+    flat_list = Cyclone.concat(multi_list)
+    assert flat_list == [1, 2, 3, 4, 5, 6]
   end
 end
